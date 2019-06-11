d7bd2d648ea09cebc52aaf4ab03920c6d3c3f625
justtacenda <46133568+justtacenda@users.noreply.github.com>
2019-06-11 10:41:52 +0530

    Add files via upload

---
+++ Even Numbers between intervals.c
@@ -0,0 +1,13 @@
+//Even 	 numbers in the interval
+#include<stdio.h>
+int main()
+{
+	int int1,int2,itr;
+	scanf("%d %d",&int1,&int2);
+	for(itr=int1+1;itr<int2;itr++)
+	{
+		if(itr%2==0)
+		printf("%d ",itr);
+	}
+	return 0;
+}
