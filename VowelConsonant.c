b0336385cae44223a397b8563dd3fe10e35b79bb
justtacenda <46133568+justtacenda@users.noreply.github.com>
2019-06-11 09:35:11 +0530

    Add files via upload

---
+++ VowelConsonant.c
@@ -0,0 +1,16 @@
+//Check if a character is vowel or consonant
+#include<stdio.h>
+int main()
+{
+	char c;
+	scanf("%c",&c);
+	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+	{
+		printf("Vowel");
+	}
+	else 
+	{
+		printf("Consonant");
+	}
+	return 0;
+}
