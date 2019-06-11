bd242d93952c4e7d6d0c647a02552f06d27cbfd0
justtacenda <46133568+justtacenda@users.noreply.github.com>
2019-06-11 13:42:52 +0530

    Add files via upload

---
+++ SortedArray.c
@@ -0,0 +1,29 @@
+//Sorted array 
+#include<stdio.h>
+int main()
+{
+	int size;
+	scanf("%d",&size);
+	int arr[size],itr,jtr,temp;
+	for(itr=0;itr<size;itr++)
+	{
+		scanf("%d",&arr[itr]);
+	}
+	for(itr=0;itr<size;itr++)
+	{
+		for(jtr=itr+1;jtr<size;jtr++)
+		{
+			if(arr[itr]>arr[jtr])
+			{
+				temp=arr[itr];
+				arr[itr]=arr[jtr];
+				arr[jtr]=temp;	
+			}
+		}
+	}
+	for(itr=0;itr<size-1;itr++)
+	{
+		printf("%d ",arr[itr]);
+	}
+	printf("%d",arr[size-1]);
+}
