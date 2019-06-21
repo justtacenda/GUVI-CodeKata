55bcdd9aa712ba680bd2ca9781f1523963311f88
justtacenda <46133568+justtacenda@users.noreply.github.com>
2019-06-21 23:27:20 +0530

    Add files via upload

---
+++ String has both alphabets and numbers.c
@@ -0,0 +1,21 @@
+//Check if String contains both alphabets and numbers
+#include<stdio.h>
+main()
+{
+	char str[1000];
+	int i,len,a_flag,n6_flag;
+	a_flag=0,n_flag=0;
+	scanf("%[^\n]",str);
+	for(len=0;str[len];len++);
+	for(i=0;i<len;i++)
+	{
+		if(isalpha(str[i]))
+		a_flag=1;
+		else if(isdigit(str[i]))
+		n_flag=1;
+	}
+	if(a_flag&&n_flag)
+	printf("yes");
+	else
+	printf("no");
+}
