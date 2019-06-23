9329508e237d5c33899c0491e71d8d25fde22182
justtacenda <46133568+justtacenda@users.noreply.github.com>
2019-06-23 19:44:38 +0530

    Add files via upload

---
+++ Print Numbers from String.c
@@ -0,0 +1,13 @@
+//Print numbers from the AlphaNumeric String
+#include<stdio.h>
+int main()
+{
+	int i,len;
+	char s[1000];
+	scanf("%s",s);
+	for(len=0;s[len];len++);
+	for(i=0;i<len;i++)
+	if(s[i]>='0'&&s[i]<='9')
+	printf("%c",s[i]);
+	return 0;
+}
