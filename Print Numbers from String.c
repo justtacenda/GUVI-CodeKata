b35d5e57555666afbab501a5759d113b280a88a3
justtacenda <46133568+justtacenda@users.noreply.github.com>
2019-06-23 19:52:20 +0530

    Update Print Numbers from String.c

--- Print Numbers from String.c
+++ Print Numbers from String.c
@@ -2,14 +2,18 @@
 #include<stdio.h>
 int main()
 {
-	int i,len;
+	int i,len,flag;
+	flag=0;
 	char s[1000];
 	scanf("%s",s);
 	for(len=0;s[len];len++);
 	for(i=0;i<len;i++)
 	if(s[i]>='0'&&s[i]<='9')
-	printf("%c",s[i]);
-	else
-	printf("");	
+	{
+		printf("%c",s[i]);
+		flag=1;
+	}
+	if(flag==0)
+		printf("");
 	return 0;
 }
